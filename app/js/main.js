038bcabf707497c0e60788753a67c592ceb372ec
Vadim Losenkov <vprog0729@gmail.com>
2021-03-16 12:40:02 +0300

    start footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 $(function() {
-  
-  
-  
-  
+  $('.reviews__wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: true,
+    prevArrow: '<div class="arrow arrow-prev"></div>',
+    nextArrow: '<div class="arrow"></div>',
+    autoplay: true
+  })
 })
