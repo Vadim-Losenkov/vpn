42592d33a2647623f051677c34d817b424e28c80
Vadim Losenkov <vprog0729@gmail.com>
2021-03-19 20:00:12 +0300

    finish project

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,38 @@ $(function() {
     dots: true,
     prevArrow: '<div class="arrow arrow-prev"></div>',
     nextArrow: '<div class="arrow"></div>',
-    autoplay: true
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+    ]
+  })
+  $('.header__btn').on('click', () => {
+    $('.header__btn').toggleClass('open')
+    $('.over').toggleClass('open')
+    $('.header__mobile').toggleClass('open')
+  })
+  $('.header__list-link').on('click', () => {
+    $('.header__btn').removeClass('open')
+    $('.over').removeClass('open')
+    $('.header__mobile').removeClass('open')
+  })
+  $('.over').on('click', () => {
+    $('.header__btn').removeClass('open')
+    $('.over').removeClass('open')
+    $('.header__mobile').removeClass('open')
   })
 })
