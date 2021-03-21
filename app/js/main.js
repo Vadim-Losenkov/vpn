edbe281b7f692e2bdc050ef6511b4c3cba4fd01f
Vadim Losenkov <vprog0729@gmail.com>
2021-03-21 20:50:15 +0300

    added popups

--- app/js/main.js
+++ app/js/main.js
@@ -40,15 +40,66 @@ $(function() {
     $('.over').removeClass('open')
     $('.header__mobile').removeClass('open')
   })
+  $('.header__login-signin').on('click', () => {
+    $('.header__btn').removeClass('open')
+    $('.over').removeClass('open')
+    $('.header__mobile').removeClass('open')
+  })
+  $('.header__login-signup').on('click', () => {
+    $('.header__btn').removeClass('open')
+    $('.over').removeClass('open')
+    $('.header__mobile').removeClass('open')
+  })
+  
+  
+  $('.top__content-btn').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+  $('.header__login-signin').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+  $('.header__login-signup').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+  $('.product__item-btn').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+  $('.reviews__banner-btn').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
 })
-
-$('.top__content-btn').magnificPopup({
-  type: 'inline',
-  removalDelay: 500, //delay removal by X to allow out-animation
-  callbacks: {
-      beforeOpen: function () {
-          this.st.mainClass = this.st.el.attr('data-effect');
-      }
-  },
-  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-});
